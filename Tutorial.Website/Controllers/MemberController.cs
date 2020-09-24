636aecee9916621ddaedaaad230061ec92c4a85a
Harun Barbasy <Harunbarbasy@hotmail.com>
2020-09-24 17:08:25 +0700

    Add project files.

---
+++ Tutorial.Website/Controllers/MemberController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq.Dynamic;
+using System.Data.Entity;
+using Tutorial.Website.Models;
+
+namespace Tutorial.Website.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LoadData()
+        {
+            try
+            {
+                //Creating instance of DatabaseContext class  
+                using (SOURCEQUEENEntities _context = new SOURCEQUEENEntities())
+                {
+                    var draw = Request.Form.GetValues("draw").FirstOrDefault();
+                    var start = Request.Form.GetValues("start").FirstOrDefault();
+                    var length = Request.Form.GetValues("length").FirstOrDefault();
+                    var sortColumn = Request.Form.GetValues("columns[" + Request.Form.GetValues("order[0][column]").FirstOrDefault() + "][name]").FirstOrDefault();
+                    var sortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+                    var searchValue = Request.Form.GetValues("search[value]").FirstOrDefault();
+
+
+                    //Paging Size (10,20,50,100)    
+                    int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                    int skip = start != null ? Convert.ToInt32(start) : 0;
+                    int recordsTotal = 0;
+
+                    // Getting all Member data    
+                    var dataMember = (from tempmember in _context.Members
+                                        select tempmember);
+                  
+                    //Sorting    
+                    if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDir)))
+                    {
+                        dataMember = dataMember.OrderBy(string.Format("{0} {1}", sortColumn, sortColumn));
+                    }
+                    //Search    
+                    if (!string.IsNullOrEmpty(searchValue))
+                    {
+                        dataMember = dataMember.Where(m => m.Name == searchValue);
+                    }
+
+                    //total number of rows count     
+                    recordsTotal = dataMember.Count();
+                    //Paging     
+                    var data = dataMember.Skip(skip).Take(pageSize).ToList();
+                    //Returning Json Data    
+                    return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data });
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
